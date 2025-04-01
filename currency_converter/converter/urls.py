e817be4062c86d4997d74dea55c8f10f1aa81616
Ugwoke Edward Chinedu <nedu.stack@gmail.com>
2025-04-01 11:08:12 +0100

    Project URL configurations and Team members tasks

--- currency_converter/converter/urls.py
+++ currency_converter/converter/urls.py
@@ -5,4 +5,8 @@ app_name = 'converter'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('convert/', views.convert, name='convert'),  # Add this line
+    #path('convert/<str:from_currency>/<str:to_currency>/<float:amount>/', views.convert, name='convert'),  # Add this line
+    #path('convert/<str:from_currency>/<str:to_currency>/', views.convert, name='convert'),  # Add this line
+    #path('convert/<str:from_currency>/', views.convert, name='convert'),  # Add this line
 ]
--- currency_converter/currency_converter/settings.py
+++ currency_converter/currency_converter/settings.py
@@ -41,9 +41,6 @@ INSTALLED_APPS = [
     #My apps
     'converter',
 
-    #Third party apps
-    'rest_framework',
-    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -126,8 +123,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
