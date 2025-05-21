27620fd8f8672c1aecc4592be356fcb43a64e97f
Ugwoke Edward Chinedu <nedu.stack@gmail.com>
2025-05-21 15:50:13 +0100

    User Dashboard Update

--- currency_converter/converter/urls.py
+++ currency_converter/converter/urls.py
@@ -6,11 +6,8 @@ app_name = 'converter'
 urlpatterns = [
     path('', views.index, name='index'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
-    #path('login', views.login, name='login'),
-    #path('register', views.register, name='register'),
-    path('convert/', views.convert, name='convert'), 
-    path('convert_view/', views.convert_view, name='convert_view'), 
-    #path('convert/<str:from_currency>/<str:to_currency>/<float:amount>/', views.convert, name='convert'),  # Add this line
-    #path('convert/<str:from_currency>/<str:to_currency>/', views.convert, name='convert'),  # Add this line
-    #path('convert/<str:from_currency>/', views.convert, name='convert'),  # Add this line
+    path('convert/', views.convert, name='convert'),
+    
+    # Make sure this URL pattern accepts parameters:
+    path('convert_view/<str:from_currency>/<str:to_currency>/', views.convert_view, name='convert_view'),
 ]
